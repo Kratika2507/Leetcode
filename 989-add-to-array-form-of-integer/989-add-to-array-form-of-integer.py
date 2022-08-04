94275875755cdd7b520d4594022c705fc5813f0e
Kratika2507 <65715379+Kratika2507@users.noreply.github.com>
2022-08-05 01:59:09 +0530

    Time: 572 ms (25.96%), Space: 14.4 MB (96.92%) - LeetHub

---
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.py
@@ -0,0 +1,23 @@
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        
+        
+        n = " "
+        
+        for i in num:
+            n = n+ str(i)
+            
+        new=int(n)
+        sum=new+k
+        return str(sum)
+            
+            
+        
+             
+        
+            
+            
+            
+        
+            
+        
