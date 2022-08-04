fa8bb5dfc6240d6509f255847db3b8a96cb4eb36
Kratika2507 <65715379+Kratika2507@users.noreply.github.com>
2022-08-05 01:34:51 +0530

    Time: 45 ms (86.23%), Space: 13.8 MB (89.68%) - LeetHub

---
+++ 415-add-strings/415-add-strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        
+        res=[]
+        first=int(num1)
+        second=int(num2)
+        
+        res= first+second
+        return str(res)
+      
+    
+            
+            
+        
